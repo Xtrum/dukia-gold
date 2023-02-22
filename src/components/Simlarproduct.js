3118d5e760f7a2c41056f40d9eec2460b6625b2d
Okonnah Victoria <victoriaokonnah@gmail.com>
2023-02-22 17:14:06 -0600

    product page added

---
+++ src/components/Simlarproduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Simlarproduct = () => {
+  return (
+    <div className='bg-blue h-[30vh] text-white'>Simlarproduct</div>
+  )
+}
+
+export default Simlarproduct
--- src/pages/Goldcoins.js
+++ src/pages/Goldcoins.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import Breadcrumb from '../components/Breadcrumb'
+import React from 'react';
+import Breadcrumb from '../components/Breadcrumb';
+import Simlarproduct from '../components/Simlarproduct';
+import {data} from '../Data/GoldcoinData'
 const Goldcoins = () => {
   return (
-    <>
-    <Breadcrumb/>
-    <div>Goldcoins</div>
-    </>
-    
+    <main>
+      <div className='w-[90%] mx-auto'>
+        <Breadcrumb/>
+        <div className='py-[2rem] grid gap-[2rem] sm:grid-cols-2 lg:grid-cols-3 justify-center sm:justify-between items-center'>
+          {data.map(gold=>{
+            return (
+              <div key={gold.id} className='sm:w-[100%] h-[23rem] shadow-md mx-auto flex flex-col items-center justify-around px-[4rem] rounded-md'>
+                <div className='grid justify-center items-center'>
+                  <img src={gold.image} alt={gold.title} className='w-[15rem] h-[15rem] object-contain'/>
+                  <h5>{gold.heading}</h5>
+                  <button className='bg-blue py-2 text-white rounded-md'>Add to cart</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <Simlarproduct/>
+    </main>
   )
 }
 
